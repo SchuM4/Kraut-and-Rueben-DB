3a74e9036f71a32a33e29a5bb5085fc871714a25
Danylo Kykosh <dkykosh@novomind.com>
2026-06-25 10:07:11 +0200

    Project init

---
+++ src/main/java/com/jmditsolutions/krautundrueben/KrautUndRuebenApplication.java
@@ -0,0 +1,13 @@
+package com.jmditsolutions.krautundrueben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrautUndRuebenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrautUndRuebenApplication.class, args);
+    }
+
+}
