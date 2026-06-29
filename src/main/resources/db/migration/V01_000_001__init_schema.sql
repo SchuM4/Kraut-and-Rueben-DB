a4de9d7407f8bd3162ac61dd3f3aefb219117f23
Danylo Kykosh <dkykosh@novomind.com>
2026-06-29 14:44:47 +0200

    Fixed the init migration accordingly to the new ERD

--- src/main/resources/db/migration/V01_000_001__init_schema.sql
+++ src/main/resources/db/migration/V01_000_001__init_schema.sql
@@ -23,9 +23,49 @@ CREATE TABLE bezugseinheit
     einheit         VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE naehrstoffangaben
+CREATE TABLE ernaehrungskategorie
 (
-    naehrstoffnr          INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ernaehrungskategorienr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    bezeichnung            VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE allergen
+(
+    allergenenr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    bezeichnung VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE kunde
+(
+    kundennr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vorname VARCHAR(100) NOT NULL,
+    nachname VARCHAR(150) NOT NULL,
+    geburtsdatum DATE,
+    emailadresse VARCHAR(255) NOT NULL UNIQUE,
+    telefonnummer VARCHAR(30)
+);
+
+-- Supplier side
+
+CREATE TABLE lieferant
+(
+    lieferantennr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    lieferantenname VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    telefon VARCHAR(30),
+    strasse VARCHAR(100) NOT NULL,
+    hausnummer VARCHAR(10) NOT NULL,
+    plz VARCHAR(10) NOT NULL REFERENCES ort(plz)
+);
+
+-- Ingredients/Recipes
+
+CREATE TABLE zutat
+(
+    zutatennr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    lieferantennr INTEGER NOT NULL REFERENCES lieferant(lieferantennr),
+    bezeichnung VARCHAR(255) NOT NULL,
+    bestand NUMERIC(10,2) NOT NULL DEFAULT 0,
     protein_g             NUMERIC(8, 2),
     kohlenhydrate_g       NUMERIC(8, 2),
     zucker_g              NUMERIC(8, 2),
@@ -39,98 +79,44 @@ CREATE TABLE naehrstoffangaben
     bezugseinheitnr       INTEGER NOT NULL REFERENCES bezugseinheit (bezugseinheitnr)
 );
 
-CREATE TABLE ernaehrungskategorie
-(
-    ernaehrungskategorienr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    bezeichnung            VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE allergene
-(
-    allergenenr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    bezeichnung VARCHAR(100) NOT NULL
-);
-
--- Supplier side
-
-CREATE TABLE lieferantenkontaktinfo
-(
-    lieferantenkontaktinfonr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR(255),
-    telefon VARCHAR(30),
-    strasse VARCHAR(100) NOT NULL,
-    hausnummer VARCHAR(10) NOT NULL,
-    plz VARCHAR(10) NOT NULL REFERENCES ort(plz)
-);
-
-CREATE TABLE lieferant
-(
-    lieferantennr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    lieferantenname VARCHAR(255) NOT NULL,
-    lieferantenkontaktinfonr INTEGER UNIQUE NOT NULL REFERENCES lieferantenkontaktinfo(lieferantenkontaktinfonr)
-);
-
--- Ingredients/Recipes
-
-CREATE TABLE zutat
-(
-    zutatennr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    lieferantennr INTEGER NOT NULL REFERENCES lieferant(lieferantennr),
-    bezeichnung VARCHAR(255) NOT NULL,
-    bestand NUMERIC(10,2) NOT NULL DEFAULT 0,
-    ernaehrungskategorienr INTEGER REFERENCES ernaehrungskategorie(ernaehrungskategorienr),
-    allergenenr INTEGER REFERENCES allergene(allergenenr),
-    naehrstoffnr INTEGER UNIQUE REFERENCES naehrstoffangaben(naehrstoffnr)
-);
-
 CREATE TABLE rezept
 (
     rezeptnr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     bezeichnung VARCHAR(150) NOT NULL,
     beschreibung TEXT,
     portionen INTEGER NOT NULL DEFAULT 1,
-    ernaehrungskategorienr INTEGER REFERENCES ernaehrungskategorie(ernaehrungskategorienr),
-    allergenenr INTEGER REFERENCES allergene(allergenenr),
-    naehrstoffnr INTEGER UNIQUE REFERENCES naehrstoffangaben(naehrstoffnr)
+    bild VARCHAR(255)
 );
 
-CREATE TABLE rezept_zutat
+CREATE TABLE zutat_ernaehrungskategorie
 (
-    rezeptnr INTEGER NOT NULL REFERENCES rezept(rezeptnr),
-    zutatnr INTEGER NOT NULL REFERENCES zutat(zutatennr),
-    menge NUMERIC(10,2) NOT NULL,
-    einheit VARCHAR(20),
-    PRIMARY KEY(rezeptnr, zutatnr)
+    zutatennr INTEGER NOT NULL REFERENCES zutat(zutatennr),
+    ernaehrungskategorienr INTEGER NOT NULL REFERENCES ernaehrungskategorie(ernaehrungskategorienr),
+    PRIMARY KEY(zutatennr, ernaehrungskategorienr)
 );
 
--- Customers
-
-CREATE TABLE kunde
+CREATE TABLE zutat_allergen
 (
-    kundennr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    vorname VARCHAR(100) NOT NULL,
-    nachname VARCHAR(150) NOT NULL,
-    geburtsdatum DATE,
-    emailadresse VARCHAR(255) NOT NULL UNIQUE,
-    telefonnummer VARCHAR(30)
+    zutatennr INTEGER NOT NULL REFERENCES zutat(zutatennr),
+    allergennr INTEGER NOT NULL REFERENCES allergen(allergenenr),
+    PRIMARY KEY(zutatennr, allergennr)
 );
 
-CREATE TABLE lieferadresse
+CREATE TABLE adresse
 (
     lieferadressenr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     strasse VARCHAR(100) NOT NULL,
     hausnummer VARCHAR(10) NOT NULL,
     plz VARCHAR(10) NOT NULL REFERENCES ort(plz),
-    kundennr INTEGER NOT NULL REFERENCES kunde(kundennr) ON DELETE CASCADE
+    kundennr INTEGER NOT NULL REFERENCES kunde(kundennr) ON DELETE CASCADE,
+    is_rechnungsadresse BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE rechnungsadresse
+CREATE TABLE zahlungsinfo
 (
-    rechnungsadressenr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    strasse VARCHAR(100) NOT NULL,
-    hausnummer VARCHAR(10) NOT NULL,
-    plz VARCHAR(10) NOT NULL REFERENCES ort(plz),
-    kundennr INTEGER UNIQUE NOT NULL REFERENCES kunde(kundennr) ON DELETE CASCADE
+    zahlungsinfonr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    zahlungsart VARCHAR(100) NOT NULL,
+    kundennr INTEGER NOT NULL REFERENCES kunde(kundennr) ON DELETE CASCADE
 );
 
 -- Orders/Pyments
@@ -141,15 +127,6 @@ CREATE TABLE bestellung
     kundennr INTEGER NOT NULL REFERENCES kunde(kundennr),
     bestelldatum DATE NOT NULL DEFAULT CURRENT_DATE,
     rechnungsbetrag NUMERIC(10,2) NOT NULL DEFAULT 0
-);
-
-CREATE TABLE zahlungsinfo
-(
-    zahlungsinfonr INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    zahlungsart VARCHAR(100) NOT NULL,
-    zahlungsstatus VARCHAR(100) NOT NULL,
-    zahlungsdatum DATE,
-    bestellungsnr INTEGER NOT NULL REFERENCES bestellung(bestellungsnr) ON DELETE CASCADE
 );
 
 CREATE TABLE bestellung_zutat
@@ -179,28 +156,32 @@ CREATE TABLE einkaufsauftrag
     nettopreis NUMERIC(10,2) NOT NULL
 );
 
+-- Customers
+
+CREATE TABLE rezept_zutat
+(
+    rezeptnr INTEGER NOT NULL REFERENCES rezept(rezeptnr),
+    zutatnr INTEGER NOT NULL REFERENCES zutat(zutatennr),
+    menge NUMERIC(10,2) NOT NULL,
+    einheit VARCHAR(20),
+    PRIMARY KEY(rezeptnr, zutatnr)
+);
+
 CREATE INDEX idx_ort_stadtnr ON ort(stadt_nr);
-CREATE INDEX idx_lieferantenkontaktinfo_plz ON lieferantenkontaktinfo(plz);
-CREATE INDEX idx_lieferant_kontaktinfonr ON lieferant(lieferantenkontaktinfonr);
+CREATE INDEX idx_lieferant_plz ON lieferant(plz);
 CREATE INDEX idx_zutat_lieferantennr ON zutat(lieferantennr);
-CREATE INDEX idx_zutat_ernaehrungskategorienr ON zutat(ernaehrungskategorienr);
-CREATE INDEX idx_zutat_allergenenr ON zutat(allergenenr);
-CREATE INDEX idx_zutat_naehrstoffnr ON zutat(naehrstoffnr);
-CREATE INDEX idx_rezepte_ernaehrungskategorienr ON rezept(ernaehrungskategorienr);
-CREATE INDEX idx_rezepte_allergenenr ON rezept(allergenenr);
-CREATE INDEX idx_rezepte_naehrstoffnr ON rezept(naehrstoffnr);
-CREATE INDEX idx_rezeptzutat_rezeptnr ON rezept(rezeptnr);
-CREATE INDEX idx_rezeptzutat_zutatnr ON rezept_zutat(zutatnr);
-CREATE INDEX idx_lieferadresse_kundennr ON lieferadresse(kundennr);
-CREATE INDEX idx_lieferadresse_plz ON lieferadresse(plz);
-CREATE INDEX idx_rechnungsadresse_kundennr ON rechnungsadresse(kundennr);
-CREATE INDEX idx_rechnungsadresse_plz ON rechnungsadresse(plz);
+CREATE INDEX idx_zutat_bezugseinheitnr ON zutat(bezugseinheitnr);
+CREATE INDEX idx_zutat_ernaehrungskategorie_kategorienr ON zutat_ernaehrungskategorie(ernaehrungskategorienr);
+CREATE INDEX idx_zutat_allergen_allergennr ON zutat_allergen(allergennr);
+CREATE INDEX idx_adresse_plz ON adresse(plz);
+CREATE INDEX idx_adresse_kundennr ON adresse(kundennr);
+CREATE INDEX idx_zahlungsinfo_kundennr ON zahlungsinfo(kundennr);
 CREATE INDEX idx_bestellung_kundennr ON bestellung(kundennr);
-CREATE INDEX idx_zahlungsinfo_bestellungsnr ON zahlungsinfo(bestellungsnr);
 CREATE INDEX idx_bestellung_zutat_zutatnr ON bestellung_zutat(zutatnr);
 CREATE INDEX idx_bestellung_rezept_rezeptnr ON bestellung_rezept(rezeptnr);
 CREATE INDEX idx_einkaufsauftrag_lieferantennr ON einkaufsauftrag(lieferantennr);
 CREATE INDEX idx_einkaufsauftrag_zutatennr ON einkaufsauftrag(zutatennr);
-CREATE INDEX idx_naehrstoffangaben_bezugseinheitnr ON naehrstoffangaben(bezugseinheitnr);
+CREATE INDEX idx_rezept_zutat_zutatnr ON rezept_zutat(zutatnr);
+CREATE INDEX idx_kunde_nachname ON kunde(nachname);
 
 COMMIT;
